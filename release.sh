fba36c27f225a48d932f6a52807fcfdd6565f224
Petr Mrázek <peterix@gmail.com>
2015-03-31 13:09:46 +0200

    Fix and adapt scripts for new build environment

--- release.sh
+++ release.sh
@@ -5,6 +5,10 @@ set -e
 ROOT=$PWD
 OUTPUT=$ROOT/build
 
+LCONVERT_BIN=${LCONVERT_BIN:-lconvert-qt5}
+LRELEASE_BIN=${LRELEASE_BIN:-lrelease-qt5}
+LUPDATE_BIN=${LUPDATE_BIN:-lupdate-qt5}
+
 if [ ! -d $OUTPUT ]
 then
 	mkdir $OUTPUT
@@ -16,9 +20,9 @@ do
 	# gets everything up to the first dot
 	lang=$(echo $po_file | grep -oP "^[^\.]*")
 	echo "    Converting $po_file to $lang.ts"
-	lconvert-qt5 -locations relative $po_file -o $lang.ts
+	$LCONVERT_BIN -locations relative $po_file -o $lang.ts
 	echo "    Create $lang.qm"
-	lrelease-qt5 $lang.ts -qm $OUTPUT/$lang.qm
+	$LRELEASE_BIN $lang.ts -qm $OUTPUT/$lang.qm
 done
 
 ls $OUTPUT/ | grep -v index > $OUTPUT/index
